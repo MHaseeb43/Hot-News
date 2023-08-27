ff02080fd3d2885979c14ba4ae7691616945fe1b
Haseeb <haseeb4304@gmail.com>
2023-08-27 15:00:49 +0500

    Add Inifinte Scroll Bar

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NewItem from './NewItem';
 import Spinner from './Spinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
     Capitalize = (str) => {
@@ -10,8 +11,10 @@ export default class News extends Component {
         super(props);
         this.state = {
             articles: [],
-            loading: false,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
+
         };
         document.title = `${this.Capitalize(this.props.category)} - Hot News`
     }
@@ -42,35 +45,53 @@ export default class News extends Component {
         this.handle();
     };
 
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 })
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7d9c470d0a8e4c4b80cce67ad574efe2&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults
+        });
+    }
+
     render() {
         return (
-            <div className='container my-3'>
+            <>
                 <div className="text-center" style={{ margin: '23px 0px' }}>
                     <h2>Hot News - {this.Capitalize(this.props.category)} TopHeadlines </h2>
                 </div>
-                {this.state.loading && <Spinner />}
-                <div className="row my-3">
-                    {this.state.articles.map((element) => {
-                        return (
-                            <div className="col-md-4" key={element.url}>
-                                <NewItem
-                                    title={element.title ? element.title : ""}
-                                    description={element.description ? element.description : ""}
-                                    imageUrl={element.urlToImage ? element.urlToImage : "https://c.ndtvimg.com/2023-08/1lcojs6_pak-cable_625x300_22_August_23.jpg"}
-                                    Newsurl={element.url}
-                                    date={element.publishedAt}
-                                    author={element.author}
-                                    source={element.source.name}
-                                />
-                            </div>
-                        );
-                    })}
-                </div>
-                <div className="container d-flex justify-content-between">
-                    <button disabled={this.state.page <= 1} type="button" onClick={this.handlePrepage} className="btn btn-dark">&larr; Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" onClick={this.handleNextpage} className="btn btn-dark">Next &rarr;</button>
-                </div>
-            </div>
+                {/* {this.state.loading && <Spinner />} */}
+
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className="container">
+                        <div className="row my-3">
+                            {this.state.articles.map((element) => {
+                                return (
+                                    <div className="col-md-4" key={element.url}>
+
+                                        <NewItem
+                                            title={element.title ? element.title : ""}
+                                            description={element.description ? element.description : ""}
+                                            imageUrl={element.urlToImage ? element.urlToImage : "https://c.ndtvimg.com/2023-08/1lcojs6_pak-cable_625x300_22_August_23.jpg"}
+                                            Newsurl={element.url}
+                                            date={element.publishedAt}
+                                            author={element.author}
+                                            source={element.source.name}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </InfiniteScroll>
+            </>
         );
     }
 }
